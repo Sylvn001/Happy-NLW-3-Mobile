8a22a231fef714bb5ba5a4bad34eaa58612b3054
Sylvn001 <juniorbaarros@gmail.com>
2020-10-18 23:20:38 -0300

    details in localhost server

--- Happy/src/services/api.ts
+++ Happy/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.106:3333'
+    baseURL: 'http://192.168.0.106:3333' // <- Example is here. if you restar the computer this url can change :D
+    // You may change this http, if you are running for android emulator or ios, you can change to localhost::3333 else you can use expo url, but change 'expo:' and the port
+    
 })
 
 export default api; 
--- Webservice/src/views/images_view.ts
+++ Webservice/src/views/images_view.ts
@@ -4,7 +4,9 @@ export default{
     render(image: Image){
         return {
             id: image.id,
-            url: `http://192.168.0.106:3333/uploads/${image.path}`,     
+            url: `http://192.168.0.106:3333/uploads/${image.path}`,     //// <- Example is here. if you restar the computer this url can change :D
+    // You may change this http, if you are running for android emulator or ios, you can change to localhost::3333 else you can use expo url, but change 'expo:' and the port
+            
         }
     },
     renderMany(images: Image[]){
